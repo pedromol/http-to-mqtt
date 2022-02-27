a19338b8ab0a2b7c2ac0d33a806a4584e34510f9
Pedro Mol <pedro@mol.net.br>
2022-02-27 09:23:17 -0300

    feat/add health path;

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 if (!process.env['MQTT_HOST']) throw new Error('Missing MQTT_HOST env variable');
+const MQTT_HOST = process.env["MQTT_HOST"];
+const WEB_PORT = process.env["HTTP_PORT"] ?? 3000;
 
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 
-const mqtt = require('mqtt');
+const mqtt = require("mqtt");
 
-const mqttClient = mqtt.connect(`mqtt://${process.env['MQTT_HOST']}`);
+const mqttClient = mqtt.connect(`mqtt://${MQTT_HOST}`);
+
+const healthTopic = "health/http-to-mqtt";
+
+mqttClient.on("connect", () => {
+  console.log(`MQTT connected to ${MQTT_HOST}`);
+
+  console.log(`Starting web server at port ${WEB_PORT}`);
+  http.createServer(handler).listen(WEB_PORT);
+});
 
 const handler = (req, res) => {
-    const { topic, message, retain } = url.parse(req.url, true).query;
+  if (req?.url === "/health") {
+    mqttClient.publish(healthTopic, 'OK', undefined, (err, packet) => {
+      if (err) {
+        res.writeHead(503, { "Content-Type": "text/plain" });
+      }
+      res.end();
+    });
+    return;
+  }
 
-    if (!topic || !message) {
-        res.writeHead(400, { 'Content-Type': 'text/plain' });
-        return;
-    }
+  const { topic, message, retain } = url.parse(req.url, true).query;
 
-    mqttClient.publish(topic, message, retain ? { retain: true } : undefined);
+  if (!topic || !message) {
+    res.writeHead(400, { "Content-Type": "text/plain" });
     res.end();
-}
+    return;
+  }
 
-http.createServer(handler).listen(process.env['HTTP_PORT'] ?? 3000);
+  mqttClient.publish(topic, message, retain ? { retain: true } : undefined, (err, packet) => {
+    res.end();
+  });
+};
+
+mqttClient.publish(healthTopic, 'OK', undefined, (err) => {
+  if (err) console.error(err);
+});
